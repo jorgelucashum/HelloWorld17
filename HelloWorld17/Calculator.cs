3a99283b6b237d5bfe38a699f0dbce8f258618bf
jorgeucas lucas <jorge_dofus1@hotmail.com>
2021-03-19 14:55:21 -0300

    Add project files.

---
+++ HelloWorld17/Calculator.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace HelloWorld17
+{
+    class Calculator
+    {
+        //params:
+        public static int Sum(params int[] numbers) // colocar 'params' antes de declarar o array vai avisar pro compilador que a função pode receber uma quantia variável de valores.
+        {
+            // Método para somar todos os valores.
+            int sum = 0; 
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+
+    }
+}
---
+++ HelloWorld17/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1 = Calculator.Sum(new int[] { 2, 3 }); // Forma compacta de instanciar um vetor e colocar os dados nele.
+            int s2 = Calculator.Sum(3, 5, 4, 1); // usando o 'params' na class, não é preciso mais instanciar o array, colocando somente os números entre os parênteses '()'.
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+
+
+            Console.ReadLine();
+        }
+    }
+}
